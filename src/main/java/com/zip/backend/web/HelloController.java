892ab6292e9a2a46f1be14c030c4b0356b6b9eb4
DongJunLee <superstring77@gmail.com>
2022-07-21 20:47:09 +0900

    7/21 Entity 및 UserRepository 생성

--- src/main/java/com/zip/backend/web/HelloController.java
+++ src/main/java/com/zip/backend/web/HelloController.java
@@ -1,9 +1,12 @@
 package com.zip.backend.web;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController  // @Controller 에 @ResponseBody 붙인 것
+@RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/")
@@ -14,5 +17,10 @@ public class HelloController {
     @GetMapping("/restricted")
     public String restricted() {
         return "to see this text you need to be logged in";
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello2(){
+        return ResponseEntity.ok("hello");
     }
 }
